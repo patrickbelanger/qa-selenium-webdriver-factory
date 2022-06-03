451e4bb623e031c7635d0d88a81b60ad0d43c04e
Patrick Bélanger <1848500+patrickbelanger@users.noreply.github.com>
2022-06-02 20:28:25 -0400

    * Added comment headers

--- src/main/java/qa/patrick/belanger/selenium/webdriver/factory/drivers/cloud/SauceLabs.java
+++ src/main/java/qa/patrick/belanger/selenium/webdriver/factory/drivers/cloud/SauceLabs.java
@@ -1,3 +1,20 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package qa.patrick.belanger.selenium.webdriver.factory.drivers.cloud;
 
 import java.net.MalformedURLException;
@@ -10,6 +27,12 @@ import org.openqa.selenium.remote.AbstractDriverOptions;
 import qa.patrick.belanger.selenium.webdriver.factory.drivers.Browser;
 import qa.patrick.belanger.selenium.webdriver.utils.Environment;
 
+/**
+ * Sauce Labs class 
+ * Returns a RemoteWebDriver connected to Sauce Labs
+ * 
+ * @author pbelanger <1848500+patrickbelanger@users.noreply.github.com>
+ */
 public class SauceLabs extends Browser {
 
 	private static final String SAUCELAB_ACCESS_KEY = "SAUCELAB_ACCESS_KEY";
@@ -39,7 +62,7 @@ public class SauceLabs extends Browser {
 	}
 	
 	/**
-	 * Creates a new WebDriver (connected on SauceLabs hub) with the specified options.
+	 * Creates a new WebDriver (connected on Sauce Labs hub) with the specified options.
 	 */
 	@Override
 	public WebDriver getWebDriver() {
